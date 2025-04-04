87b5709970eb189ae0b364e56c76ab1851b6d88d
A1ame <94046520+A1ame@users.noreply.github.com>
2025-04-04 15:53:29 +0500

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,1 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkZXfWV5_pQQVT1J1ytce87po_ap3dE-c",
+  authDomain: "ar-fit-ad561.firebaseapp.com",
+  databaseURL: "https://ar-fit-ad561-default-rtdb.firebaseio.com",
+  projectId: "ar-fit-ad561",
+  storageBucket: "ar-fit-ad561.firebasestorage.app",
+  messagingSenderId: "625031688221",
+  appId: "1:625031688221:web:31e767e30272f83ed2d6f4",
+  measurementId: "G-E3YN43WT6R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Authentication
+export const db = getFirestore(app);
+export const auth = getAuth(app);
